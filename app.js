8a034c9511838f0f65f05d831d707d64ef749657
brian25cruz2006 <brianemanuelcruz25@gmail.com>
2023-08-20 17:31:15 -0300

    nuevos cambios

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 const path = require('path');
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 
 app.listen(PORT, ()=>{
@@ -22,3 +24,14 @@ app.get("/login", (req,res)=>{
     res.sendFile(__dirname + "/views/login.html");
 });
 
+app.post('/procesarregistro', (req, res) =>{
+    //procesar datos que lleguen al formulario
+    console.log(req.body)
+    //redirigir a la pagina home
+    res.redirect("/")
+});
+
+app.post('/login', (req,res) =>{
+    console.log(req.body)
+    res.redirect("/")
+})
